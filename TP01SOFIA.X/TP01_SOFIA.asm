250175b6b95445caf9aff6b509fa7635c3416b6e
Sofia Victória <71997997+Sofiavictoria@users.noreply.github.com>
2024-10-29 16:37:03 -0300

    Add files via upload

---
+++ TP01SOFIA.X/TP01_SOFIA.asm
@@ -0,0 +1,67 @@
+; Autor: Sofia Victoria Bispo da Silva
+; Matricula : 202063542
+; Materia : Eletronica Embarcada 
+
+;  --- TEST POINT 1 ---
+
+#include <xc.inc>
+
+global count	                    ; Variável global count
+PSECT udata_shr			    ; localizada na zona espelhada da memória de dados 
+count:
+    DS 1			    ; reserva uma localização de memória (1 byte) para count 
+    
+psect resetVec,class=CODE,delta=2   ; Vetor de Reset definido em xc.inc para os PIC10/12/16
+resetVec:
+    PAGESEL start		    ; Seleciona página de código de programa onde está start
+    goto start 
+
+psect code,class=CODE,delta=2	    ; Define uma secção o de código de programa 
+
+;inicializa as portas
+start:
+    BANKSEL PORTA		;Seleção do banco de memória do registrador PORTA
+    clrf PORTA			;Zera PORTA, para quando as saídas sejam configuradas os pinos fiquem em zero
+    clrf PORTB			;Limpa PORTB
+    clrf PORTC			;Limpa PORTC
+	
+    BANKSEL ANSEL               ; Seleciona o banco de memória do registrador ANSELA
+    clrf ANSEL                  ; Zera ANSELA para usar todas as portas como digitais
+    clrf ANSELH                 ; Zera ANSELH para usar todas as portas como digitais
+	
+    BANKSEL TRISA               ; Seleção do banco de memória do registrador TRISA
+    clrf    TRISA               ; Configura TRISA como saida
+    bsf	    TRISA, 5		;Configura o pino RA5 como entrada 
+
+    BANKSEL ANSEL               ; Seleciona o banco de memória do registrador ANSELA
+    clrf ANSEL                  ; Zera ANSELA para usar todas as portas como digitais
+    
+    BANKSEL TRISB               ; Seleciona o banco de memória do registrador TRISB
+    movlw 0xFF			; Carrega 0xFF (11111111 em binário) no registrador W
+    movwf TRISB      		; Configura todos os bits de PORTB como entrada
+BANKSEL ANSEL               ; Seleciona o banco de memória do registrador ANSELA
+    clrf ANSEL                  ; Zera ANSELA para usar todas as portas como digitais
+    BANKSEL TRISC               ; Seleciona o banco de memória do registrador TRISC
+    movlw 0xFF			; Carrega 0xFF (11111111 em binário) no registrador W
+    movwf TRISC      		; Configura todos os bits de PORTC como entrada
+
+
+verificaRA5:
+    clrwdt  			;Zera Can de Guarda (Watchdog Timer)
+    clrw
+    btfsc PORTA,5		; Verifica o valor do pino RA5
+    goto soma    	 	;Se RA5=1, realiza soma
+    goto subtracao		; Se RA5=0, realiza subtração
+	
+soma:
+    movf PORTB, w		;Move PORTB para W
+    addwf PORTC,w		; W= PORTB + PORTC
+    movwf PORTA			;armazena o resultado na PORTA	
+    goto verificaRA5		;Volta para verificar RA5 novamente
+	
+
+subtracao:
+	movf PORTC, w
+	subwf PORTB, w
+	movwf	PORTA		;armazena o resultado na PORTA
+	goto verificaRA5 	;Volta para verificar RA5 novamente
---
+++ TP01SOFIA.X/build/default/debug/TP01_SOFIA.d
@@ -0,0 +1,1 @@
+build/default/debug\TP01_SOFIA.i: TP01_SOFIA.asm
